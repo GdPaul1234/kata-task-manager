db1784edea31b27b1d700aaf94ca530ab5ca8a90
GdPaul1234 <paul.godin1234@outlook.fr>
2022-11-15 17:44:36 +0100

    05 - Change Task Status

--- src/main/java/info/dmerej/Task.java
+++ src/main/java/info/dmerej/Task.java
@@ -2,8 +2,60 @@ package info.dmerej;
 
 import info.dmerej.enums.TaskStatus;
 
-public record Task(String description, int id, TaskStatus status) {
+import java.util.Objects;
+
+public final class Task {
+    private final String description;
+    private final int id;
+
+    private TaskStatus status;
+
+    public Task(String description, int id, TaskStatus status) {
+        this.description = description;
+        this.id = id;
+        this.status = status;
+    }
+
     public Task(String description, int id) {
         this(description, id, TaskStatus.TODO);
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public TaskStatus status() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Task) obj;
+        return Objects.equals(this.description, that.description) &&
+                this.id == that.id &&
+                Objects.equals(this.status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, id, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Task[" +
+                "description=" + description + ", " +
+                "id=" + id + ", " +
+                "status=" + status + ']';
     }
 }
--- src/test/java/info/dmerej/TaskManagerTest.java
+++ src/test/java/info/dmerej/TaskManagerTest.java
@@ -2,6 +2,8 @@ package info.dmerej;
 
 import info.dmerej.enums.TaskStatus;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.List;
 
