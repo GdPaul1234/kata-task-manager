ebfb840824d445343f449bec84dbc534c07cc4fe
GdPaul1234 <paul.godin1234@outlook.fr>
2022-11-15 17:11:53 +0100

    02 - Add more add task test

--- src/test/java/info/dmerej/CommandTest.java
+++ src/test/java/info/dmerej/CommandTest.java
@@ -9,12 +9,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommandTest {
     @Test
-    void command_add_task() {
+    void command_add_task_to_empty_list() {
         var taskManager = new TaskManager();
 
         taskManager.addCreatedTask("Ma nouvelle tâche");
 
         var expectedCreatedTask = new Task("Ma nouvelle tâche", 1, TaskStatus.TODO);
         assertEquals(taskManager.tasks, List.of(expectedCreatedTask));
+    }
+
+    @Test
+    void command_add_task_to_non_empty_list() {
+        var taskManager = new TaskManager();
+
+        taskManager.addCreatedTask("Ma nouvelle tâche");
+        taskManager.addCreatedTask("Ma deuxième tâche");
+
+        var expectedTaskList = List.of(
+                new Task("Ma nouvelle tâche", 1, TaskStatus.TODO),
+                new Task("Ma deuxième tâche", 2, TaskStatus.TODO)
+        );
+        assertEquals(taskManager.tasks, expectedTaskList);
     }
 }
